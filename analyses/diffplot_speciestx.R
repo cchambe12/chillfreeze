9653e7b6f5dbb0fd83c98f2a1c548763b18720ed
cchambe12 <cchamberlain@g.harvard.edu>
2019-06-17 21:45:19 -0400

    adding new plots

--- analyses/diffplot_speciestx.R
+++ analyses/diffplot_speciestx.R
@@ -101,7 +101,7 @@ dvr4 <- ggplot(chill4, aes(x=dvrcontmean, y=dvrfrzmean, col=species.name), alpha
   scale_y_continuous(breaks=seq(8,28,2)) +
   scale_x_continuous(breaks=seq(8,28,2)) +
   coord_cartesian(xlim=c(8,28), ylim=c(8,28), expand=TRUE) + guides(col=FALSE) +
-  ggtitle("A. Four weeks chilling") + geom_abline(intercept = 0, slope = 1, col=grey)
+  ggtitle("A. Four weeks chilling") + geom_abline(intercept = 0, slope = 1, col="grey")
    
 dvr6 <- ggplot(chill6, aes(x=dvrcontmean, y=dvrfrzmean, col=species.name), alpha=2) + 
   geom_point(aes(x=dvrcontmean, y=dvrfrzmean, size=as.factor(diff.labels)), shape=21) + 
@@ -122,7 +122,7 @@ dvr6 <- ggplot(chill6, aes(x=dvrcontmean, y=dvrfrzmean, col=species.name), alpha
   scale_y_continuous(breaks=seq(8,28,2)) +
   scale_x_continuous(breaks=seq(8,28,2)) +
   coord_cartesian(xlim=c(8,28), ylim=c(8,28), expand=TRUE) + guides(col=FALSE) +
-  ggtitle("B. Six weeks chilling") + geom_abline(intercept = 0, slope = 1, col=grey)
+  ggtitle("B. Six weeks chilling") + geom_abline(intercept = 0, slope = 1, col="grey")
 
 dvr8 <- ggplot(chill8, aes(x=dvrcontmean, y=dvrfrzmean, col=species.name), alpha=2) + 
   geom_point(aes(x=dvrcontmean, y=dvrfrzmean, size=as.factor(diff.labels)), shape=21) + 
@@ -143,7 +143,7 @@ dvr8 <- ggplot(chill8, aes(x=dvrcontmean, y=dvrfrzmean, col=species.name), alpha
   scale_y_continuous(breaks=seq(8,28,2)) +
   scale_x_continuous(breaks=seq(8,28,2)) +
   coord_cartesian(xlim=c(8,28), ylim=c(8,28), expand=TRUE) +
-  ggtitle("C. Eight weeks chilling") + geom_abline(intercept = 0, slope = 1, col=grey)
+  ggtitle("C. Eight weeks chilling") + geom_abline(intercept = 0, slope = 1, col="grey")
 
 
 sizelegend <- ggplot(aes(x=dvrcontmean, y=dvrfrzmean, col=species.name, size=as.factor(diff.labels)), data=tt) + 
@@ -185,3 +185,132 @@ dvrplot<-grid.arrange(dvr4, dvr6, dvr8, mylegend, nrow=4, heights = c(4, 0.55, 0
 ggsave("figures/dvrspeciesdifftest.png",width=30, height=14,units="cm",bg = "white",dpi=500, plot=dvrplot)
 
 
+##### Now for some bar plots with error bars, ordered by day of budburst #####
+dvrbar <- subset(chillfrz, select=c("species", "chill", "dvr", "tx", "budburst"))
+dvrbar <- na.omit(dvrbar)
+dvrbar <- dvrbar[!duplicated(dvr),]
+dvrbar$tx <-ifelse(dvrbar$tx==0, "control", "treatment")
+
+dvrbar$species.name <- NA
+dvrbar$species.name <- ifelse(dvrbar$species=="ACESAC", "Acer saccharinum", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="ALNRUG", "Alnus rugosa", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="BETPAP", "Betula papyrifera", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="BETPOP", "Betula populifolia", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="CORRAC", "Cornus racemosa", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="SALPUR", "Salix purpurea", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="SORAME", "Sorbus americana", dvrbar$species.name)
+dvrbar$species.name <- ifelse(dvrbar$species=="VIBDEN", "Viburnum dentatum", dvrbar$species.name)
+
+dvrbar$code <- reorder(dvrbar$species, dvrbar$budburst)
+chill.bars4 <- dvrbar[(dvrbar$chill==1),]
+chill.bars6 <- dvrbar[(dvrbar$chill==2),]
+chill.bars8 <- dvrbar[(dvrbar$chill==3),]
+
+chill.bars4$dvrmean <- ave(chill.bars4$dvr, chill.bars4$tx, chill.bars4$species)
+chill.bars4$dvrsd <- ave(chill.bars4$dvr, chill.bars4$tx, chill.bars4$species, FUN=sd)
+chill.bars4$ymin <- chill.bars4$dvrmean-chill.bars4$dvrsd
+chill.bars4$ymax <- chill.bars4$dvrmean+chill.bars4$dvrsd
+chill.bars4$meancont <- mean(chill.bars4$dvr[chill.bars4$tx=="control"]) # 16.0
+chill.bars4$meantx <- mean(chill.bars4$dvr[chill.bars4$tx=="treatment"]) # 18.8
+
+dvrbar4 <- ggplot(chill.bars4, aes(x=code, y=dvrmean, fill=code, alpha=tx)) + 
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=ymin, ymax=ymax),width = 0.2, position=position_dodge(0.9)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        legend.text.align = 0,
+        legend.position = "none",
+        axis.text.x = element_text(face = "italic", angle=45, hjust=1),
+        legend.key = element_rect(colour = "transparent", fill = "white")) +
+  xlab("") + 
+  ylab("Duration of vegetative risk") + 
+  scale_fill_manual(name="Species", values=cols,
+                    labels=chill.bars4$code) +
+  geom_hline(aes(yintercept = meancont), col="black", alpha=0.3, linetype="dashed") +
+  geom_hline(aes(yintercept = meantx), col="black", alpha=1, linetype="dashed") +
+  #scale_y_continuous(breaks = sort(c(seq(min(5), max(35), by=5), 16.0, 18.8))) +
+  scale_x_discrete(labels=c("ACESAC"="Acer saccharinum",
+                              "ALNRUG"="Alnus rugosa",
+                              "BETPAP"="Betula papyrifera",
+                              "BETPOP"="Betula populifolia",
+                              "CORRAC"="Cornus racemosa", 
+                              "SALPUR"="Salix purpurea",
+                              "SORAME"="Sorbus americana",
+                              "VIBDEN"="Viburnum dentatum")) +
+  scale_alpha_manual(name="Treatments", values=c(0.1, 1), labels=chill.bars4$tx) +
+  coord_cartesian(xlim=c(1, 8), ylim=c(5,35), expand=TRUE) + guides(fill=FALSE) +
+  ggtitle("A. Four weeks chilling") 
+
+chill.bars6$dvrmean <- ave(chill.bars6$dvr, chill.bars6$tx, chill.bars6$species)
+chill.bars6$dvrsd <- ave(chill.bars6$dvr, chill.bars6$tx, chill.bars6$species, FUN=sd)
+chill.bars6$ymin <- chill.bars6$dvrmean-chill.bars6$dvrsd
+chill.bars6$ymax <- chill.bars6$dvrmean+chill.bars6$dvrsd
+chill.bars6$meancont <- mean(chill.bars6$dvr[chill.bars6$tx=="control"]) #15.4
+chill.bars6$meantx <- mean(chill.bars6$dvr[chill.bars6$tx=="treatment"]) #17.5
+
+dvrbar6 <- ggplot(chill.bars6, aes(x=code, y=dvrmean, fill=code, alpha=tx)) + 
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=ymin, ymax=ymax),width = 0.2, position=position_dodge(0.9)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        legend.text.align = 0,
+        legend.position = "none",
+        axis.text.x = element_text(face = "italic", angle=45, hjust=1),
+        legend.key = element_rect(colour = "transparent", fill = "white")) +
+  xlab("") + 
+  ylab("Duration of vegetative risk") + 
+  geom_hline(aes(yintercept = meancont), col="black", alpha=0.3, linetype="dashed") +
+  geom_hline(aes(yintercept = meantx), col="black", alpha=1, linetype="dashed") +
+  #scale_y_continuous(breaks = sort(c(seq(min(5), max(35), length.out=5), 15.4, 17.5))) +
+  scale_fill_manual(name="Species", values=cols,
+                    labels=chill.bars4$code) +
+  scale_x_discrete(labels=c("ACESAC"="Acer saccharinum",
+                            "ALNRUG"="Alnus rugosa",
+                            "BETPAP"="Betula papyrifera",
+                            "BETPOP"="Betula populifolia",
+                            "CORRAC"="Cornus racemosa", 
+                            "SALPUR"="Salix purpurea",
+                            "SORAME"="Sorbus americana",
+                            "VIBDEN"="Viburnum dentatum")) +
+  scale_alpha_manual(name="Treatments", values=c(0.1, 1), labels=chill.bars6$tx) +
+  coord_cartesian(xlim=c(1, 8), ylim=c(5,35), expand=TRUE) + guides(fill=FALSE) +
+  ggtitle("B. Six weeks chilling") 
+
+
+chill.bars8$dvrmean <- ave(chill.bars8$dvr, chill.bars8$tx, chill.bars8$species)
+chill.bars8$dvrsd <- ave(chill.bars8$dvr, chill.bars8$tx, chill.bars8$species, FUN=sd)
+chill.bars8$ymin <- chill.bars8$dvrmean-chill.bars8$dvrsd
+chill.bars8$ymax <- chill.bars8$dvrmean+chill.bars8$dvrsd
+chill.bars8$meancont <- mean(chill.bars8$dvr[chill.bars8$tx=="control"]) #13.2
+chill.bars8$meantx <- mean(chill.bars8$dvr[chill.bars8$tx=="treatment"]) #16.9
+
+dvrbar8 <- ggplot(chill.bars8, aes(x=code, y=dvrmean, fill=code, alpha=tx)) + 
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_errorbar(aes(ymin=ymin, ymax=ymax),width = 0.2, position=position_dodge(0.9)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        legend.text.align = 0,
+        #legend.position = "none",
+        axis.text.x = element_text(face = "italic", angle=45, hjust=1),
+        legend.key = element_rect(colour = "transparent", fill = "white")) +
+  xlab("") + 
+  ylab("Duration of vegetative risk") + 
+  geom_hline(aes(yintercept = meancont), col="black", alpha=0.3, linetype="dashed") +
+  geom_hline(aes(yintercept = meantx), col="black", alpha=1, linetype="dashed") +
+  #scale_y_continuous(breaks = sort(c(seq(min(5), max(35), length.out=5), 13.2, 16.9))) +
+  scale_fill_manual(name="Species", values=cols,
+                    labels=chill.bars4$code) +
+  scale_x_discrete(labels=c("ACESAC"="Acer saccharinum",
+                            "ALNRUG"="Alnus rugosa",
+                            "BETPAP"="Betula papyrifera",
+                            "BETPOP"="Betula populifolia",
+                            "CORRAC"="Cornus racemosa", 
+                            "SALPUR"="Salix purpurea",
+                            "SORAME"="Sorbus americana",
+                            "VIBDEN"="Viburnum dentatum")) +
+  scale_alpha_manual(name="", values=c(0.1, 1), labels=chill.bars8$tx) +
+  coord_cartesian(xlim=c(1, 8), ylim=c(5,35), expand=TRUE) + guides(fill=FALSE) +
+  ggtitle("C. Eight weeks chilling")
+
+
+quartz()
+dvrbarplot <- grid.arrange(dvrbar4, dvrbar6, dvrbar8, ncol=3, widths=c(1, 1, 1.4))
+
+ggsave("figures/dvrspeciesbarplot.png",width=30, height=12,units="cm",bg = "white",dpi=500, plot=dvrbarplot)
