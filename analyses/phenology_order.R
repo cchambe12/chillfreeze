71bf3048d06ca6d8423e6d938079faccaa297372
cchambe12 <cchamberlain@g.harvard.edu>
2020-05-05 17:27:57 -0400

    lots of updates to outline

--- analyses/phenology_order.R
+++ analyses/phenology_order.R
@@ -329,3 +329,106 @@ rankadj_bytx_bset <- ggplot(bbandgs_rankfix_bset, aes(y=newranks, x=txchill, col
 
 quartz()
 rankadj_bytx_bset
+
+
+
+
+
+############### Add in budburst for the supp:
+
+#### Let's look at overall averages and then do barplots... I think that will be the best first stab
+bbandgs$lobyspp <- ave(bbandgs$budburst, bbandgs$species)
+bbandgs$code <- reorder(bbandgs$species, bbandgs$lobyspp) ## Levels: SALPUR CORRAC BETPAP BETPOP ALNRUG VIBDEN SORAME ACESAC
+
+### Okay, now we're going to try and rank them based on order by treatment by species
+# Step 1)
+bbandgs$lobytxchill <- ave(bbandgs$budburst, bbandgs$species, bbandgs$txchill)
+
+bbandgs$species_tx <- paste(bbandgs$species, bbandgs$txchill, sep="_")
+bbandgs$code <- reorder(bbandgs$species_tx, bbandgs$lobytxchill)
+
+
+bbandgs$rank <- rank(bbandgs$lobytxchill, bbandgs$species_tx)
+
+bbandgs.sub <- subset(bbandgs, select=c("rank", "txchill", "species"))
+bbandgs.sub <- bbandgs.sub[!duplicated(bbandgs.sub),]
+
+bbandgs.sub <- arrange(bbandgs.sub, rank)
+bbandgs.sub$rank_order <- 1:nrow(bbandgs.sub)
+
+# Step 2) Plot using this rank
+cols <- colorRampPalette(brewer.pal(8,"Dark2"))(8)
+rankbytx <- ggplot(bbandgs.sub, aes(y=rank_order, x=txchill, col=species)) + 
+  geom_line(aes(group=species)) + 
+  coord_cartesian(ylim=c(0, 55)) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        legend.text.align = 0,
+        legend.position = "none",
+        legend.key = element_rect(colour = "transparent", fill = "white"),
+        legend.text = element_text(face="italic")) +
+  xlab("") + 
+  ylab("Order of budburst") +  
+  scale_color_manual(name="Species", values=cols,
+                     labels=c("ACESAC"="Acer saccharinum",
+                              "ALNRUG"="Alnus rugosa",
+                              "BETPAP"="Betula papyrifera",
+                              "BETPOP"="Betula populifolia",
+                              "CORRAC"="Cornus racemosa", 
+                              "SALPUR"="Salix purpurea",
+                              "SORAME"="Sorbus americana",
+                              "VIBDEN"="Viburnum dentatum")) +
+  scale_x_discrete(labels=c("10"="Control x \n4wks Chill",
+                            "11"="False Spring x \n4wks Chill",
+                            "20"="Control x \n6wks Chill",
+                            "21"="False Spring x \n6wks Chill",
+                            "30"="Control x \n8wks Chill",
+                            "31"="False Spring x \n8wks Chill")) + scale_y_continuous(expand=c(0,0))
+
+
+#quartz()
+#rankbytx
+
+#### Now let's look at raw budburst data by treatments
+budburstbytx <- ggplot(bbandgs, aes(y=budburst, x=txchill, col=species)) +  geom_jitter(width=0.2) +
+  theme(panel.background = element_blank(), axis.line = element_line(colour = "black"),
+        legend.text.align = 0,
+        legend.position="none",
+        legend.key = element_rect(colour = "transparent", fill = "white"),
+        legend.text = element_text(face="italic")) +
+  xlab("") + 
+  ylab("Day of budburst") +  
+  scale_color_manual(name="Species", values=cols,
+                     labels=c("ACESAC"="Acer saccharinum",
+                              "ALNRUG"="Alnus rugosa",
+                              "BETPAP"="Betula papyrifera",
+                              "BETPOP"="Betula populifolia",
+                              "CORRAC"="Cornus racemosa", 
+                              "SALPUR"="Salix purpurea",
+                              "SORAME"="Sorbus americana",
+                              "VIBDEN"="Viburnum dentatum")) +
+  scale_x_discrete(labels=c("10"="Control x \n4wks Chill",
+                            "11"="False Spring x \n4wks Chill",
+                            "20"="Control x \n6wks Chill",
+                            "21"="False Spring x \n6wks Chill",
+                            "30"="Control x \n8wks Chill",
+                            "31"="False Spring x \n8wks Chill")) 
+
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)}
+
+mylegend<-g_legend(budburstbytx)
+
+
+#quartz()
+
+png("figures/budburst_orderandraw.png", ### makes it a nice png and saves it so it doesn't take forever to load as a pdf!
+    width=12,
+    height=4.5, units="in", res = 350 )
+
+grid.arrange(rankbytx, budburstbytx, mylegend, ncol=3, widths=c(1.2,1.2,0.35))
+dev.off()
+
+
